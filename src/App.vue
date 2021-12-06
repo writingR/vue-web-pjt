5fd35c3f3b8e23b9b37b47f4cd85aa12d771803a
writingR <writing0625@gmail.com>
2021-12-06 15:27:55 +0900

    vue alias setting

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js App" />
+  <AppHeader></AppHeader>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from "@/components/HelloWorld.vue";
+import AppHeader from "@/components/AppHeader.vue";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
-  },
-  created() {
-    console.log("hi");
+    AppHeader,
   },
 };
 </script>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h2>test</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
