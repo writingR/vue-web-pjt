6161edc3c218938924a21d0e5e0de53957117e75
writingR <writing0625@gmail.com>
2021-12-08 16:17:00 +0900

    signup page 로직 반영

--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -10,16 +10,19 @@
         <input type="text" id="password" v-model="password" />
       </div>
       <div>
-        <label for="nickname">nicname: </label>
+        <label for="nickname">nickname: </label>
         <input type="text" id="nickname" v-model="nickname" />
       </div>
       <button type="submit">회원가입</button>
       <button type="reset">취소</button>
+      <p>{{ logMessage }}</p>
     </form>
   </div>
 </template>
 
 <script>
+import { registerUser } from '@/api/index';
+
 export default {
   name: 'SignUpForm',
   data() {
@@ -27,11 +30,26 @@ export default {
       username: '',
       password: '',
       nickname: '',
+      logMessage: '',
     };
   },
   methods: {
-    submitForm() {
+    async submitForm() {
       console.log('submit');
+      const userData = {
+        username: this.username,
+        password: this.password,
+        nickname: this.nickname,
+      };
+      const response = await registerUser(userData);
+      console.log(response.data.username);
+      this.logMessage = `${response.data.username}님이 가입되었습니다.`;
+      this.initForm();
+    },
+    initForm() {
+      this.username = '';
+      this.password = '';
+      this.nickname = '';
     },
   },
 };
--- src/views/SignUpPage.vue
+++ src/views/SignUpPage.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <h1>회원가입</h1>
     <SignUpForm></SignUpForm>
   </div>
 </template>
