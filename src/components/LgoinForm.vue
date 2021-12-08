7fa23314bdc642a30d8620ef11e19fac8485e74c
writingR <writing0625@gmail.com>
2021-12-08 18:14:39 +0900

    Login 유효성 검사

--- src/components/LgoinForm.vue
+++ src/components/LgoinForm.vue
@@ -8,14 +8,18 @@
       <label for="password">pw: </label>
       <input type="password" id="password" v-model="password" />
     </div>
-    <button type="submit">로그인</button>
-    <button type="reset">취소</button>
+    <button v-bind:disabled="!isUsernameValid || !password" type="submit">
+      로그인
+    </button>
+    <button disabled type="button"><a href="/signup">회원가입</a></button>
+    <!--    <button type="reset">취소</button>-->
     <p>{{ logMessage }}</p>
   </form>
 </template>
 
 <script>
 import { loginUser } from '@/api/index';
+import { validateEmail } from '@/utils/validation';
 
 export default {
   data() {
@@ -45,6 +49,11 @@ export default {
     initForm() {
       this.username = '';
       this.password = '';
+    },
+  },
+  computed: {
+    isUsernameValid() {
+      return validateEmail(this.username);
     },
   },
 };
---
+++ src/utils/validation.js
@@ -0,0 +1,8 @@
+// 이메일 유효성검사
+function validateEmail(email) {
+  var re =
+    /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+  return re.test(email);
+}
+
+export { validateEmail };
